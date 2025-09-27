2626a8e65143edc8c7b3b016367eac4e01c03463
RafaelGasparoto <rafaelgasparoto4@gmail.com>
2025-09-27 19:17:56 -0300

    Log

--- src/main/java/br/edu/utfpr/td/tsi/web/scraper/etl/Job.java
+++ src/main/java/br/edu/utfpr/td/tsi/web/scraper/etl/Job.java
@@ -2,7 +2,11 @@ package br.edu.utfpr.td.tsi.web.scraper.etl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Job<T, O> {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	private Extrator<T> extrator;
 	private Transformador<T, O> transformador;
@@ -21,14 +25,27 @@ public class Job<T, O> {
 	}
 
 	public void executar() {
-
 		List<T> lista = extrator.extrair();
+		
+		if(lista.size() == 0) {
+			this.logger.info("Nenhum dado encontrado");
+		}
+		
+		this.logger.info("Iniciando transformação de dados de " + lista.getFirst().getClass().getSimpleName());
 
 		for (T item : lista) {
 			transformador.transformar(item);
 		}
-		carregador.carregar(lista);
+		
+		this.logger.info("Transformação de " + lista.getFirst().getClass().getSimpleName() + " finalizado");
 
+
+		this.logger.info("Iniciando gravação de dados de " + lista.getClass().getSimpleName());
+
+		carregador.carregar(lista);
+		
+		this.logger.info("Gravação de " + lista.getFirst().getClass().getSimpleName() + " finalizado");
+		
 	}
 
 }
